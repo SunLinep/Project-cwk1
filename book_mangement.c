cac358e87305216a55f6f226f433f5eee03d7857
SunLinep <96744742+SunLinep@users.noreply.github.com>
2022-03-19 11:40:05 +0800

    1

--- book_mangement.c
+++ book_mangement.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "book_mangement.h"
+#include "book_management.h"
 
-extern Book * bookfirst = (Book*)malloc(sizeof Book);
+Book * bookfirst;
 
 int store_books(FILE *file){
 	if(bookfirst->id != 1);
 }
 
-\\1说明该文件为空文件
 int load_books(FILE *file){
 	int i = 0;
 	char *content;
@@ -17,7 +16,7 @@ int load_books(FILE *file){
 	if(fgets(content,1000,file) == NULL){
 		return 1;
 	}else{
-		Book * p = (Book*)malloc(sizeof Book);
+		Book * p = (Book*)malloc(sizeof p);
 		p = bookfirst;
 		do{
 			while (content[i] != '\n') i++;
@@ -27,53 +26,53 @@ int load_books(FILE *file){
 			p->authors = strtok(NULL, s);
 			p->year = atoi(strtok(NULL, s));
 			p->copies = atoi(strtok(NULL, s));
-			Book * l= (Book*)malloc(sizeof Book);
+			Book * l= (Book*)malloc(sizeof l);
 			l->next = p->next;
 			p->next = l;
 			p = l;
 			memset(content, '\0', sizeof(content));
-		}while(fgets(content, 405, book) != NULL)
-		l = l->next;
+		}while(fgets(content, 405, file) != NULL);
 		free(p);
-		free(l);
 	}
 	return 0;
 }
 
 int add_book(Book book){
 	int i = 1;
-	Book * p = (Book*)malloc(sizeof Book);
+	Book * p = (Book*)malloc(sizeof p);
+    Book * h = &book;
 	p = bookfirst;
 	while(p->next != NULL){
 		i++;
-		if(strcmp(p->title, book->title) == 0 && strcmp(p->authors, book->authors) == 0){
+		if(strcmp(p->title, h->title) == 0 && strcmp(p->authors, h->authors) == 0){
 			return 1;
 		}
 		p = p->next;
 	}
-	p->next = book;
-	book->id = ++i;
-	p = book->next;
-	book = book->next;
+	p->next = h;
+	h->id = ++i;
+	p = h->next;
+	h = h->next;
 	free(p);
-	free(book);
+    free(h);
 	return 0;
 }
 
 int remove_book(Book book){
-	Book * p = (Book*)malloc(sizeof Book);
-	Book * l = (Book*)malloc(sizeof Book);
+    Book * h = &book;
+	Book * p = (Book*)malloc(sizeof p);
+	Book * l = (Book*)malloc(sizeof l);
 	p = bookfirst;
-	while(&& strcmp(p->next->title, book->title) != 0 && strcmp(p->next->authors, book->authors) != 0){
+	while(strcmp(p->next->title, h->title) != 0 && strcmp(p->next->authors, h->authors) != 0){
 		p = p->next;
 		if(p == NULL) return 1;
 	}
-	book = p->next;
+	h = p->next;
 	l = p->next->next;
 	p->next = l;
-	p = p->next
+	p = p->next;
 	free(l);
-	free(book);
+	free(h);
 	while(p != NULL){
 		p->id--;
 		p = p->next;
@@ -84,10 +83,9 @@ int remove_book(Book book){
 
 BookList find_book_by_title (const char *title){
 	int i = 0;
-	Book * p = (Book*)malloc(sizeof Book);
-	Book * h = (Book*)malloc(sizeof Book);
-	Book * (l->list) = (Book*)malloc(sizeof Book);
-	BookList * l = (BookList*)malloc(sizeof BookList);
+	Book * p = (Book*)malloc(sizeof p);
+	Book * h = (Book*)malloc(sizeof h);
+	BookList * l = (BookList*)malloc(sizeof l);
 	p = bookfirst;
 	while(p != NULL){
 		if(strcmp(p->title, title) == 0){
@@ -118,83 +116,81 @@ BookList find_book_by_title (const char *title){
 	h = NULL;
 	free(h);
 	l->length=i;
-	return l;
+	return *l;
 }
 
 BookList find_book_by_author (const char *author){
-	int i = 0;
-	Book * p = (Book*)malloc(sizeof Book);
-	Book * h = (Book*)malloc(sizeof Book);
-	Book * (l->list) = (Book*)malloc(sizeof Book);
-	BookList * l = (BookList*)malloc(sizeof BookList);
-	p = bookfirst;
-	while(p != NULL){
-		if(strcmp(p->author, author) == 0){
-			l->list->id = p->id;
-			l->list->year = p->year;
-			l->list->copies = p->copies;
-			l->list->title = p->title;
-			l->list->authors = p->authors;
-			h = l->list->next;
-			p = p->next;
-			i++;
-			break;
-		}
-		p = p->next;
-	}
-	while(p != NULL){
-		if(strcmp(p->author, author) == 0){
-			h->id = p->id;
-			h->year = p->year;
-			h->copies = p->copies;
-			h->title = p->title;
-			h->authors = p->authors;
-			h = h->next;
-			i++;
-		}
-		p = p->next;
-	}
-	h = NULL;
-	free(h);
-	l->length=i;
-	return l;
+    int i = 0;
+    Book * p = (Book*)malloc(sizeof p);
+    Book * h = (Book*)malloc(sizeof h);
+    BookList * l = (BookList*)malloc(sizeof l);
+    p = bookfirst;
+    while(p != NULL){
+        if(strcmp(p->authors, author) == 0){
+            l->list->id = p->id;
+            l->list->year = p->year;
+            l->list->copies = p->copies;
+            l->list->title = p->title;
+            l->list->authors = p->authors;
+            h = l->list->next;
+            p = p->next;
+            i++;
+            break;
+        }
+        p = p->next;
+    }
+    while(p != NULL){
+        if(strcmp(p->authors, author) == 0){
+            h->id = p->id;
+            h->year = p->year;
+            h->copies = p->copies;
+            h->title = p->title;
+            h->authors = p->authors;
+            h = h->next;
+            i++;
+        }
+        p = p->next;
+    }
+    h = NULL;
+    free(h);
+    l->length=i;
+    return *l;
 }
 
 BookList find_book_by_year (unsigned int year){
-	int i = 0;
-	Book * p = (Book*)malloc(sizeof Book);
-	Book * h = (Book*)malloc(sizeof Book);
-	Book * (l->list) = (Book*)malloc(sizeof Book);
-	BookList * l = (BookList*)malloc(sizeof BookList);
-	p = bookfirst;
-	while(p != NULL){
-		if(p->year == year){
-			l->list->id = p->id;
-			l->list->year = p->year;
-			l->list->copies = p->copies;
-			l->list->title = p->title;
-			l->list->authors = p->authors;
-			h = l->list->next;
-			p = p->next;
-			i++;
-			break;
-		}
-		p = p->next;
-	}
-	while(p != NULL){
-		if(p->year == year){
-			h->id = p->id;
-			h->year = p->year;
-			h->copies = p->copies;
-			h->title = p->title;
-			h->authors = p->authors;
-			h = h->next;
-			i++;
-		}
-		p = p->next;
-	}
-	h = NULL;
-	free(h);
-	l->length=i;
-	return l;
+    int i = 0;
+    Book * p = (Book*)malloc(sizeof p);
+    Book * h = (Book*)malloc(sizeof h);
+    BookList * l = (BookList*)malloc(sizeof l);
+    p = bookfirst;
+    while(p != NULL){
+        if(p->year==year){
+            l->list->id = p->id;
+            l->list->year = p->year;
+            l->list->copies = p->copies;
+            l->list->title = p->title;
+            l->list->authors = p->authors;
+            h = l->list->next;
+            p = p->next;
+            i++;
+            break;
+        }
+        p = p->next;
+    }
+    while(p != NULL){
+        if(p->year==year){
+            h->id = p->id;
+            h->year = p->year;
+            h->copies = p->copies;
+            h->title = p->title;
+            h->authors = p->authors;
+            h = h->next;
+            i++;
+        }
+        p = p->next;
+    }
+    h = NULL;
+    free(h);
+    l->length=i;
+    return *l;
 }
--- main.c
+++ main.c
@@ -1,316 +1,322 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "book_management.h"
 
 typedef struct users{
 	char username[100];
 	char password[100];
-}user;
+    struct users *next;
+}User;
 
-Book * bookfirst = (Book*)malloc(sizeof Book);
-bookfirst->next = NULL;
-User * firstuser = (user*)malloc(sizeof User);
-firstuser->username =  "librarian";
-firstuser->password = "librarian";
-firstuser->next = NULL;
+extern Book * bookfirst;
+User * userfirst;
 char *bookfilename = "bookfile.txt";
 char *userfilename = "users.txt";
 
-int mainMenu(){
-	int option;
-	loaduserlist(userfilename);
-	printf("\nPlease choose an option:\n1) Register an accoun\n2) Login\n3) Search for books\n4) Display all books\n5) Quit\n Option:")
-	while(scanf("%d",&option) != 1){
-		if(option >=1 && option <= 5) break;
-		printf("\nSorry, the option you entered was invalid, please try again.\n");
-		printf("\nPlease choose an option:\n1) Register an accoun\n2) Login\n3) Search for books\n4) Display all books\n5) Quit\n Option:")
-		fflush(stdin);
-	}
-	switch(option){
-		case 1:
-			register(userfilename);
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			displayBooks(bookfilename);
-			break;
-		case 5:
-			break;
-	}
-}
+void loaduserlist(char *userfile);
 
-void searchbook(int user){
-	int option;
-	printf("\nPlease choose an option:\n1)Find books by title\n2)Find Books by author\n3)Find books by year\n4)Return to previous menu\n Option:")
-	while(scanf("%d",&option) != 1){
-		if(option >=1 && option <= 4) break;
-		printf("\nSorry, the option you entered was invalid, please try again.\n");
-		printf("\nPlease choose an option:\n1)Find books by title\n2)Find Books by author\n3)Find books by year\n4)Return to previous menu\n Option:")
-		fflush(stdin);
-	}
-	switch(option){
-		case 1:
-			char *q;
-			char *tle = "Title";
-			char *aut = "Authors";
-			printf("Please enter title: ");
-			scanf("%s",q);
-			BookList*p = (BookList*)malloc(sizeof Booklist);
-			Book*a = (Book*)malloc(sizeof Booklist);
-			p = find_book_by_title(q);
-			a = p->list;
-			printf("ID\t%50c%50cyear\tcopies",tle,aut);
-			while(p->length > 0){
-				printf("%d\t%50c%50c%d\t%d",a->id,a->title,a->authors,a->year,a->copies);
-				a = a->next;
-			}
-			searchbook(user);
-			break;
-		case 2:
-			char *q;
-			char *tle = "Title";
-			char *aut = "Authors";
-			printf("Please enter author: ");
-			scanf("%s",q);
-			BookList*p = (BookList*)malloc(sizeof Booklist);
-			Book*a = (Book*)malloc(sizeof Booklist);
-			p = find_book_by_author(q);
-			a = p->list;
-			printf("ID\t%50c%50cyear\tcopies",tle,aut);
-			while(p->length > 0){
-				printf("%d\t%50c%50c%d\t%d",a->id,a->title,a->authors,a->year,a->copies);
-				a = a->next;
-			}
-			searchbook(user);
-			break;
-		case 3:
-			int q;
-			char *tle = "Title";
-			char *aut = "Authors";
-			printf("Please enter year: ");
-			scanf("%d",q);
-			BookList*p = (BookList*)malloc(sizeof Booklist);
-			Book*a = (Book*)malloc(sizeof Booklist);
-			p = find_book_by_year(q);
-			a = p->list;
-			printf("ID\t%50c%50cyear\tcopies",tle,aut);
-			while(p->length > 0){
-				printf("%d\t%50c%50c%d\t%d",a->id,a->title,a->authors,a->year,a->copies);
-				a = a->next;
-			}
-			searchbook(user);
-			break;
-		case 4:
-		printf("Returning to previous menu...");
-			if(user){
-				login_librarian();
-			}else{
-				login_user();
-			}
-			break;
-	}
-}
+void displayBooks(char *bookfile);
 
-void login_librarian(void){
-	int option;
-	printf("\n(logged in as: librarian)\nPlease choose an option:\n1) Add a book\n2) Remove a book\n3) Search for books\n4) Display all books\n5) log out\n Option:")
-	while(scanf("%d",&option) != 1){
-		if(option >=1 && option <= 5) break;
-		printf("\nSorry, the option you entered was invalid, please try again.\n");
-		printf("\nPlease choose an option:\n1) Add a book\n2) Remove a book\n3) Search for books\n4) Display all books\n5) log out\n Option:")
-		fflush(stdin);
-	}
-	switch(option){
-		case 1:
-			Book *p = (Book*)malloc(sizeof Book);
-			p->next = NULL;
-			printf("Enter the title of the book you wish to add: ");
-			scanf("%s",p->title);
-			getchar();
-			printf("Enter the author of the book you wish to add:");
-			scanf("%s",p->authors);
-			getchar();
-			printf("Enter the year that the book you wish to add was released: ");
-			scanf("%d",p->year);
-			getchar();
-			printf("Enter the number of copies of the book that you wish to add: ");
-			scanf("%d",p->copies);
-			if(add_book(p)){
-				printf("Sorry, you attempted to add an invalid book, please try agagin\n");
-			}else{
-				printf("Book was successfully added");
-			}
-			free(p);
-			break;
-		case 2:
-			Book *p = (Book*)malloc(sizeof Book);
-			p->next = NULL;
-			printf("Enter the title of the book you wish to add: ");
-			scanf("%s",p->title);
-			getchar();
-			printf("Enter the author of the book you wish to add:");
-			scanf("%s",p->authors);
-			getchar();
-			if(remove_book(p)){
-				printf("Sorry, you attempted to remove an invalid book, please try agagin\n");
-			}else{
-				printf("Book was successfully removed");
-			}
-			break;
-		case 3:
-			printf("\nLoading search menu...\n");
-			searchbook(1);
-			break;
-		case 4:
-			displayBooks(bookfilename);
-			break;
-		case 5:
-			printf("Logging out...");
-			mainMenu();
-			break;
-	}
-	login_librarian(void);
-}
+void registeruser(char *userfile);
 
-void login_user(char *name){
-	int option;
-	printf("\n(logged in as: %s)\nPlease choose an option:\n1) Add a book\n2) Remove a book\n3) Search for books\n4) Display all books\n5) log out\n Option:",name);
-	while(scanf("%d",&option) != 1){
-		if(option >=1 && option <= 5) break;
-		printf("\nSorry, the option you entered was invalid, please try again.\n");
-		printf("\nPlease choose an option:\n1) Add a book\n2) Remove a book\n3) Search for books\n4) Display all books\n5) log out\n Option:")
-		fflush(stdin);
-	}
-	switch(option){
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3:
-			printf("\nLoading search menu...\n");
-			searchbook(1);
-			break;
-		case 4:
-			displayBooks(bookfilename);
-			break;
-		case 5:
-			printf("Logging out...");
-			mainMenu();
-			break;
-	}
-	login_user(void);
-}
+int mainMenu();
 
-void login(User *user){
-	char username[100];
-	char password[100];
-	printf("\nPlease enter a username: ");
-	scanf("%s",username);
-	printf("\nPlease enter a password: ");
-	scanf("%s",password);
-	user * users = (user*)malloc(sizeof users);
-	users = firstuser;
-	if(strcmp(username, "librarian") == 0 && strcmp(password, "librarian") == 0){
-		login_librarian();
-	}else{
-		while(users != NULL){
-			if(strcmp(users->username, username) == 0 && strcmp(users->password, password) == 0){
-				login_user(users->username);
-			}
-			users = users->next;	
-		}
-	}
-	free(users);
-}
+void searchbook(char *user);
 
-void register(char *userfile);{
-	int i = 1;
-	FILE *usersfp;
-	usersfe = fopen(userfile, "w");
-	char username[100];
-	char password[100];
-	printf("\nPlease enter a username: ");
-	scanf("%s",username);
-	printf("\nPlease enter a password: ");
-	scanf("%s",password);
-	user * register = (user*)malloc(sizeof register);
-	register = firstuser;
-	while(register != NULL){
-		fprintf(usersfp,"%s\t%s\n",register->username,register->password);
-		if(strcmp(register->username, username) == 0){
-			i = 0;
-			printf("Sorry, registration unsuccessful, the username you entered already exists");
-		}
-		register = register->next;
-	}
-	if(i){
-		strcpy(register->username, username);
-		stpcpy(register->password, password);
-		printf("Registered library account successfully!");
-	}
-	free(register);
-	fclose(usersfp);
+void login_librarian(void);
+
+void login_user(char *name);
+
+void login(User *user);
+
+int main(void){
+    bookfirst = (Book*)malloc(sizeof bookfirst);
+    bookfirst->next = NULL;
+    userfirst = (User*) malloc(sizeof userfirst);
+    strcpy(userfirst->username ,"librarian");
+    strcpy( userfirst->password ,"librarian");
+    userfirst->next = NULL;
+	mainMenu();
+	return 0;
 }
 
 void loaduserlist(char *userfile){
-	int i = 0;
-	FILE * users;
-	char content[200];
-	memset(content, '\0', sizeof(content));
-	const char s[2] = "\t";
-	if((users = fopen(userfile, "r")) == NULL){
-		users = fopen(userfile, "w");
-		fprintf(users, "%s\t%s", firstuser->username, firstuser->password);
-		fclose(users);
-	}else{
-		if(fgets(content, 200, users) == NULL){
-			userfirst->username = "librarian");
-			userfirst->password = "librarian");
-			userfirst->next = NULL;
-			return -1;
-		}else{
-			user * q = (user*)malloc(sizeof user);
-			q = userfirst;
-			q->next = NULL;
-			do{
-				while (content[i] != '\n') i++;
-				content[i] = '\0';
-				q->username = strtok(content, s);
-				p->password = strtok(NULL, s);
-				user * h= (user*)malloc(sizeof user);
-				h->next = q->next;
-				q->next = h;
-				q = h;
-				free(h);
-			}while(fgets(content, 200, users) != NULL)
-		}
-	}
-	fclose(users);
+    int i = 0;
+    FILE * users;
+    char content[200];
+    memset(content, '\0', sizeof(content));
+    const char s[2] = "\t";
+    if((users = fopen(userfile, "r")) == NULL){
+        users = fopen(userfile, "w");
+        fprintf(users, "%s\t%s", userfirst->username, userfirst->password);
+        fclose(users);
+    }else{
+        if(fgets(content, 200, users) == NULL){
+            strcpy(userfirst->username, "librarian");
+            strcpy(userfirst->password, "librarian");
+            userfirst->next = NULL;
+            return;
+        }else{
+            User * q = (User*)malloc(sizeof q);
+            q = userfirst;
+            q->next = NULL;
+            do{
+                while (content[i] != '\n') i++;
+                content[i] = '\0';
+                strcpy(q->username, strtok(content, s));
+                strcpy(q->password, strtok(NULL, s));
+                User * h= (User*)malloc(sizeof h);
+                h->next = q->next;
+                q->next = h;
+                q = h;
+                free(h);
+            }while(fgets(content, 200, users) != NULL);
+        }
+    }
+    fclose(users);
 }
 
 void displayBooks(char *bookfile){
-	FILE *bookfp;
-	char *tle = "Title";
-	char *aut = "Authors";
-	Book *p = (Book*)malloc(sizeof Book);
-	p = bookfirst;
-	if((bookfp = fopen(bookfiile,"r")) != NULL){
-		if(!load_books(FILE *file)){
-			printf("ID\t%50c%50cyear\tcopies",tle,aut);
-			while(p){
-				printf("%d\t%50c%50c%d\t%d",p->id,p->title,p->authors,p->year,p->copies);
-				p = p->next;
-			}
-		}else{
-			printf("There are no books in the library");
-		}
-	}
-	mainMenu();
+    FILE *bookfp;
+    char *tle = "Title";
+    char *aut = "Authors";
+    Book *p = (Book*)malloc(sizeof p);
+    p = bookfirst;
+    if((bookfp = fopen(bookfile,"r")) != NULL){
+        if(!load_books(bookfp)){
+            printf("ID\t%50c%50cyear\tcopies",tle,aut);
+            while(p){
+                printf("%d\t%50c%50c%d\t%d",p->id,p->title,p->authors,p->year,p->copies);
+                p = p->next;
+            }
+        }else{
+            printf("There are no books in the library");
+        }
+    }
+    mainMenu();
 }
 
-int main(void){
-	mainMenu();
-	return 0;
+void registeruser(char *userfile){
+    int i = 1;
+    FILE *usersfp;
+    usersfp = fopen(userfile, "w");
+    char username[100];
+    char password[100];
+    printf("\nPlease enter a username: ");
+    scanf("%s",username);
+    printf("\nPlease enter a password: ");
+    scanf("%s",password);
+    User * regist = (User*)malloc(sizeof regist);
+    regist = userfirst;
+    while(regist != NULL){
+        fprintf(usersfp,"%s\t%s\n",regist->username,regist->password);
+        if(strcmp(regist->username, username) == 0){
+            i = 0;
+            printf("Sorry, registration unsuccessful, the username you entered already exists");
+        }
+        regist = regist->next;
+    }
+    if(i){
+        strcpy(regist->username, username);
+        strcpy(regist->password, password);
+        printf("Registered library account successfully!");
+    }
+    free(regist);
+    fclose(usersfp);
+}
+
+int mainMenu(){
+    int option;
+    loaduserlist(userfilename);
+    printf("\nPlease choose an option:\n1) Register an accoun\n2) Login\n3) Search for books\n4) Display all books\n5) Quit\n Option:");
+    while(scanf("%d",&option) != 1){
+        if(option >=1 && option <= 5) break;
+        printf("\nSorry, the option you entered was invalid, please try again.\n");
+        printf("\nPlease choose an option:\n1) Register an accoun\n2) Login\n3) Search for books\n4) Display all books\n5) Quit\n Option:");
+        fflush(stdin);
+    }
+    switch(option){
+        case 1:
+            registeruser(userfilename);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            displayBooks(bookfilename);
+            break;
+        case 5:
+            break;
+    }
+}
+
+void searchbook(char *user){
+    int option;
+    printf("\nPlease choose an option:\n1)Find books by title\n2)Find Books by author\n3)Find books by year\n4)Return to previous menu\n Option:");
+    while(scanf("%d",&option) != 1){
+        if(option >=1 && option <= 4) break;
+        printf("\nSorry, the option you entered was invalid, please try again.\n");
+        printf("\nPlease choose an option:\n1)Find books by title\n2)Find Books by author\n3)Find books by year\n4)Return to previous menu\n Option:");
+        fflush(stdin);
+    }
+    if(option == 1){
+        char *q;
+        char *tle = "Title";
+        char *aut = "Authors";
+        printf("Please enter title: ");
+        scanf("%s",q);
+        BookList p;
+        BookList *h = &p;
+        Book*a = (Book*)malloc(sizeof a);
+        p = find_book_by_title(q);
+        a = h->list;
+        printf("ID\t%50c%50cyear\tcopies",tle,aut);
+        while(h->length > 0){
+            printf("%d\t%50c%50c%d\t%d",a->id,a->title,a->authors,a->year,a->copies);
+            a = a->next;
+        }
+        searchbook(user);
+    }else if(option == 2){
+        char *q;
+        char *tle = "Title";
+        char *aut = "Authors";
+        printf("Please enter author: ");
+        scanf("%s",q);
+        BookList p;
+        BookList *h = &p;
+        Book*a = (Book*)malloc(sizeof a);
+        p = find_book_by_author(q);
+        a = h->list;
+        printf("ID\t%50c%50cyear\tcopies",tle,aut);
+        while(h->length > 0){
+            printf("%d\t%50c%50c%d\t%d",a->id,a->title,a->authors,a->year,a->copies);
+            a = a->next;
+        }
+        searchbook(user);
+    }else if(option == 3) {
+        int q;
+        char *tle = "Title";
+        char *aut = "Authors";
+        printf("Please enter year: ");
+        scanf("%d",q);
+        BookList p;
+        BookList *h = &p;
+        Book*a = (Book*)malloc(sizeof a);
+        p = find_book_by_year(q);
+        a = h->list;
+        printf("ID\t%50c%50cyear\tcopies",tle,aut);
+        while(h->length > 0){
+            printf("%d\t%50c%50c%d\t%d",a->id,a->title,a->authors,a->year,a->copies);
+            a = a->next;
+        }
+        searchbook(user);
+    }else if(option == 4) {
+        printf("Returning to previous menu...");
+        if (strcmp(user, "librarian") == 0) {
+            login_librarian();
+        }else {
+            login_user(user);
+        }
+    }
+}
+
+void login_librarian(void){
+    int option;
+    printf("\n(logged in as: librarian)\nPlease choose an option:\n1) Add a book\n2) Remove a book\n3) Search for books\n4) Display all books\n5) log out\n Option:");
+    while(scanf("%d",&option) != 1){
+        if(option >=1 && option <= 5) break;
+        printf("\nSorry, the option you entered was invalid, please try again.\n");
+        printf("\nPlease choose an option:\n1) Add a book\n2) Remove a book\n3) Search for books\n4) Display all books\n5) log out\n Option:");
+        fflush(stdin);
+    }
+    if(option == 1) {
+        Book p;
+        p.next = NULL;
+        printf("Enter the title of the book you wish to add: ");
+        scanf("%s", p.title);
+        getchar();
+        printf("Enter the author of the book you wish to add:");
+        scanf("%s", p.authors);
+        getchar();
+        printf("Enter the year that the book you wish to add was released: ");
+        scanf("%d", p.year);
+        getchar();
+        printf("Enter the number of copies of the book that you wish to add: ");
+        scanf("%d", p.copies);
+        if (add_book(p)) {
+            printf("Sorry, you attempted to add an invalid book, please try agagin\n");
+        } else {
+            printf("Book was successfully added");
+        }
+    }else if(option == 2) {
+        Book p;
+        p.next = NULL;
+        printf("Enter the title of the book you wish to add: ");
+        scanf("%s", p.title);
+        getchar();
+        printf("Enter the author of the book you wish to add:");
+        scanf("%s", p.authors);
+        getchar();
+        if (remove_book(p)) {
+            printf("Sorry, you attempted to remove an invalid book, please try agagin\n");
+        } else {
+            printf("Book was successfully removed");
+        }
+    }else if(option == 3) {
+        printf("\nLoading search menu...\n");
+        searchbook("librarian");
+    }else if(option == 4) {
+        displayBooks(bookfilename);
+    }else if(option == 5) {
+        printf("Logging out...");
+        mainMenu();
+    }
+    login_librarian();
+}
+
+void login_user(char *name){
+    int option;
+    printf("\n(logged in as: %s)\nPlease choose an option:\n1) Add a book\n2) Remove a book\n3) Search for books\n4) Display all books\n5) log out\n Option:",name);
+    while(scanf("%d",&option) != 1){
+        if(option >=1 && option <= 5) break;
+        printf("\nSorry, the option you entered was invalid, please try again.\n");
+        printf("\nPlease choose an option:\n1) Add a book\n2) Remove a book\n3) Search for books\n4) Display all books\n5) log out\n Option:");
+        fflush(stdin);
+    }
+    if(option == 1);
+    else if(option == 2);
+    else if(option == 3) {
+        printf("\nLoading search menu...\n");
+        searchbook(name);
+    }else if(option == 4) {
+        displayBooks(bookfilename);
+    }else if(option == 5) {
+        printf("Logging out...");
+        mainMenu();
+    }
+    login_user(name);
+}
+
+void login(User *user){
+    char username[100];
+    char password[100];
+    printf("\nPlease enter a username: ");
+    scanf("%s",username);
+    printf("\nPlease enter a password: ");
+    scanf("%s",password);
+    User * users = (User*)malloc(sizeof users);
+    users = userfirst;
+    if(strcmp(username, "librarian") == 0 && strcmp(password, "librarian") == 0){
+        login_librarian();
+    }else{
+        while(users != NULL){
+            if(strcmp(users->username, username) == 0 && strcmp(users->password, password) == 0){
+                login_user(users->username);
+            }
+            users = users->next;
+        }
+    }
+    free(users);
 }
